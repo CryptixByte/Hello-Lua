c334bac49c443d25a6a24105aaaf3c0cfda9a1d9
CryptixByte <168722619+CryptixByte@users.noreply.github.com>
2024-05-02 02:08:13 -0700

    Create Hello.lua

---
+++ Hello.lua
@@ -0,0 +1,7 @@
+--Basic One Line Comments are written using double dashes, which is significantly different than C and C++
+--[[
+Multi-lined commments are written using double dashes along with double open brackets and double closing
+brackets with the comment inbetween the brackets as such.
+]]
+
+print("Hello World")
