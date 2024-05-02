8183d192b3791a30ed42e06aa5fdb5135276b17e
CryptixByte <168722619+CryptixByte@users.noreply.github.com>
2024-05-02 02:46:46 -0700

    Update Hello.lua

--- Hello.lua
+++ Hello.lua
@@ -1,17 +1,50 @@
---Basic One Line Comments
+-- Basic One Line Comments
 --[[
 Multi-lined commments are used like this
 For example, I don't have to do -- every line
 ]]
 
---How To Print In Lua
+-- How To Print In Lua
 print("Hello World!")
 
---How To Assign Variables In Lua
+-- How To Assign Variables In Lua
 local hello = "Hello World!"
 local five = 5
 
 print(hello)
 print(five)
 
---How To Define Functions In Lua
+-- How To Define Functions In Lua
+local function Goodbye ()
+    print("Goodbye User!")
+end
+
+Goodbye()
+
+-- Math Examples In Lua
+local x = 3
+local y = 9
+local answerAdd = x+y
+local answerSub = y-x
+local answerMul = x*y
+local answerDiv = y/x
+
+print(answerAdd)
+print(answerSub)
+print(answerMul)
+print(answerDiv)
+
+-- How To Get User Input
+print("Pick A Number Between 1-10?")
+local userNum = io.read("*n");
+print("You Choose, "..userNum.."!")
+
+-- How To Define If Statements
+local gameAnswer = 4
+if userNum == gameAnswer then
+    print("You Picked "..userNum.." Which Is Correct!")
+else
+    print("You Picked "..userNum.." Which Was Incorrect "..gameAnswer.." Is The Correct Number")
+end
+
+--
