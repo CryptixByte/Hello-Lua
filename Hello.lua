8006d024e01124297d6d42df7b7dcada78030166
CryptixByte <168722619+CryptixByte@users.noreply.github.com>
2024-05-02 23:44:10 -0700

    Update Hello.lua

--- Hello.lua
+++ Hello.lua
@@ -8,12 +8,19 @@ For example, I don't have to do -- every line
 print("Hello World!")
 
 -- How To Assign Variables In Lua
+--[[
+Local Variables Work In Only Within The File
+Global Variables Work Globally Across Multiple Files
+]]
 local hello = "Hello World!"
 local five = 5
-
+_G.TestVariable = 15
+local one, two, three = 15, 12, 14
+print(one,two,three)
+local oneFive = TestVariable
 print(hello)
 print(five)
-
+print(oneFive)
 -- How To Define Functions In Lua
 local function Goodbye ()
     print("Goodbye User!")
@@ -28,11 +35,20 @@ local answerAdd = x+y
 local answerSub = y-x
 local answerMul = x*y
 local answerDiv = y/x
+local remainder = y%x
 
 print(answerAdd)
 print(answerSub)
 print(answerMul)
 print(answerDiv)
+print(remainder)
+print(math.floor(3.14))
+print(math.ceil(3.65))
+print(math.random())
+--Random non repeating numbers
+math.randomseed(os.time(1,100))
+print(math.random(1, 100))
+
 
 -- How To Get User Input
 print("Pick A Number Between 1-10?")
@@ -47,4 +63,29 @@ else
     print("You Picked "..userNum.." Which Was Incorrect "..gameAnswer.." Is The Correct Number")
 end
 
---
+-- How To Handle Strings
+local multiLine = [[
+Showcasing Multiline
+Strings In LUA
+]]
+print(multiLine)
+local singleLine = "Hello World"
+print(singleLine)
+local addToEnd = "Hello World! ".."Welcome To Lua!"
+print(addToEnd)
+local stringCount = "Hello World"
+print(#stringCount)
+local x = 5
+local convert = tostring(x)
+print(x,convert)
+local tabIndent = "\tHello World"
+print(tabIndent)
+print(string.lower(tabIndent))
+print(string.upper(tabIndent))
+print(string.len(tabIndent))
+-- \\ to enter a backslash \v for vertical print \t for tab indentation \" to insert quotes
+
+
+
+
+
